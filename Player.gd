ebd5c91f5ad8b986917e054b27e86e545506f8b7
BobWizardLad <bobwizardlad@Gmail.com>
2022-12-04 13:01:20 -0600

    Player Moves
    Player sprite can move in an 8-direction pattern around the screen. No momentum, no physics.

--- Player.gd
+++ Player.gd
@@ -2,15 +2,33 @@ extends RigidBody2D
 
 
 # Declare member variables here. Examples:
-var move = 1.0
+var speed = 600
+var screen_size
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var velocity = Vector2.ZERO # Player move vector
+	if(Input.is_action_pressed("move_up")):
+		velocity.y -= 1
+	if(Input.is_action_pressed("move_down")):
+		velocity.y += 1
+	if(Input.is_action_pressed("move_right")):
+		velocity.x += 1
+	if(Input.is_action_pressed("move_left")):
+		velocity.x -= 1
 	
-	pass
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
