96da9e42c196d3fa0a51414b225f21adcce63a85
BobWizardLad <bobwizardlad@Gmail.com>
2022-12-10 21:34:14 -0600

    Bullets Now Collide With Bodies
    Bullets will now colldie, and an enemy is here to be shot at! *NOTE* The player currently shoots themselves when they fire, I wanted to immortalize this.

--- Bullet.gd
+++ Bullet.gd
@@ -1,11 +1,22 @@
-extends "res://Entity.gd"
+extends RigidBody2D
 
 # Character Variables
 var timer = null
+var velocity = Vector2()
+var speed = 400
+
+signal hit
 
 # Action to take on timer timeout: Remove Sprite
 func despawn():
 	queue_free()
+
+# Function to find out what the bullet hit and remove them accordingly
+func elim_target(var1):
+	var targets = $Area2D.get_overlapping_areas()
+	for target in targets:
+		target.get_parent().queue_free()
+	despawn()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,4 +25,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
