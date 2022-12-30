1cc3b3a5e20dc5e24d4aa0e9e6116ee89f4652a8
BobWizardLad <bobwizardlad@Gmail.com>
2022-12-29 19:11:27 -0600

    It is a Game!
    A very short game nonetheless. This is the stopping point for the game jam. I may not extend beyond this, but it's out here.

--- Game.gd
+++ Game.gd
@@ -2,10 +2,7 @@ extends Node2D
 
 
 # Need all three enemies to be gone
-var goal1 = false
-var goal2 = false
-var goal3 = false
-
+var run_once = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,11 +11,13 @@ func _ready():
 
 # Check to see if the enemies are gone, if so Display you win
 func _process(delta):
-	if not self.has_node("Enemy"):
-		goal1 = true
-	if not self.has_node("Enemy3"):
-		goal2 = true
-	if not self.has_node("Enemy4"):
-		goal3 = true
-	if goal1 and goal2 and goal3:
+	print(self.get_children())
+	var nodes = [
+		self.has_node("Enemy"),
+		self.has_node('Enemy3'),
+		self.has_node('Enemy4')
+	]
+	if nodes.count(false) == len(nodes) and run_once == true:
 		$YouWin.visible = true
+		$Player.free()
+		run_once = false
